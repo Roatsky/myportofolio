6f9dd13cf07e5c3df592d76fcc014d7d9db433be
Roatsky <m.naufal1801@gmail.com>
2026-09-13 05:57:01 +0700

    Tugas 2 (menambahkan data ke model.objects)

--- main/models.py
+++ main/models.py
@@ -35,7 +35,6 @@ class Education(models.Model):
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
-    description = models.TextField()
     category = models.CharField(max_length=20, choices=Education_CHOICES, default='non-formal')
     thumbnail = models.URLField(blank=True, null=True)
     started_at = models.DateTimeField(auto_now_add=True)
@@ -57,11 +56,6 @@ class Skills(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     category = models.CharField(max_length=20, choices=Education_CHOICES, default='soft-skills')
-    started_at = models.DateTimeField(auto_now_add=True)
-    ended_at = models.DateTimeField(blank=True, null=True)
     def __str__(self):
         return self.title
-    
-    @property
-    def is_ongoing(self):
-        return self.ended_at is None
+
