36d93cdde368f989cb383a27721eed9d6f19e323
Roatsky <m.naufal1801@gmail.com>
2026-09-16 23:38:38 +0700

    Tutorial 3 (final bug fix)

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_education, show_skills, create_project, show_project
+from main.views import show_main, show_experience, show_education, show_skills, create_project, show_project, get_projects_json, delete_project
 
 app_name = "main"
 
@@ -11,4 +11,6 @@ urlpatterns = [
     path("education/", show_education, name="show_education"),
     path("projects/add/", create_project, name="create_project"),
     path("projects/", show_project, name="show_project"),
+    path("api/projects/", get_projects_json, name="get_projects_json"),
+    path("projects/<uuid:project_id>/delete/",delete_project,name="delete_project"),
 ]
--- main/views.py
+++ main/views.py
@@ -21,29 +21,35 @@ def show_main(request):
 
 def show_experience(request):
     context = {
-        "name": "Muhammad Naufal Syarifuddin",
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
 
 def show_education(request):
     context = {
-        "name": "Muhammad Naufal Syarifuddin",
         "education_list": Education.objects.all(),
     }
     return render(request, "education.html", context)
 
 def show_skills(request):
     context = {
-        "name": "Muhammad Naufal Syarifuddin",
         "skills_list": Skills.objects.all(),
     }
     return render(request, "skills.html", context)
 
 def show_project(request):
+    json_response = get_projects_json(request)
+
+    projects = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+    projects = [project.object for project in projects]
+    title_query = request.GET.get("title", "").strip()
+
     context = {
-        "name": "Burhan",
-        "project_list": Project.objects.all(),
+        "project_list": projects,
+        "title_query": title_query,
     }
     return render(request, "project.html", context)
 
@@ -53,10 +59,30 @@ def create_project(request):
     if request.method == "POST" and form.is_valid():
         form.save()
         messages.success(request, "Proyek baru berhasil ditambahkan!")
-        return redirect("main:show_projects")
+        return redirect("main:show_project")
 
     context = {
         "name": "Burhan",
         "form": form,
     }
     return render(request, "projects_form.html", context)
+
+def get_projects_json(request):
+    title_query = request.GET.get("title", "").strip()
+    projects = Project.objects.all()
+
+    if title_query:
+        projects = projects.filter(title__icontains=title_query)
+
+    projects_json = serializers.serialize("json", projects)
+    return HttpResponse(projects_json, content_type="application/json")
+
+def delete_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+
+    if request.method == "POST":
+        project.delete()
+        messages.success(request, "Project berhasil dihapus!")
+        return redirect("main:show_project")
+
+    return redirect("main:show_project")
